95e0ca076c91a1f1dd5c4f1e84bd1a4017f4faca
yzyievbyfqfhk4901hd <viceatvici@proton.me>
2026-07-13 21:05:24 +0300

    initial

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import './App.css'
+  import Dinah from './lib/Dinah/Dinah.svelte'
+
+  document.title = 'cat dinah'
+</script>
+
+<div class="app-shell">
+  <div class="app">
+    <Dinah />
+  </div>
+</div>
---
+++ src/lib/Dinah/Dinah.svelte
@@ -0,0 +1,7 @@
+<script>
+  import './Dinah.css'
+</script>
+
+<section class="cat-screen" aria-label="Cat">
+    <img class="cat-image" src="/images/dinah.png" alt="cheshire" />
+</section>
